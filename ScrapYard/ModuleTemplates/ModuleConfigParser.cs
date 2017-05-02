49b94adc3984b1ec8d3bfc11ddacec23e31810b1
magico13 <magico1313@gmail.com>
2017-05-01 23:11:59 -0400

    Start of more advanced module definitions. Now references MagiCore, will require it once I enable the new code.

---
+++ ScrapYard/ModuleTemplates/ModuleConfigParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrapYard
+{
+    /// <summary>
+    /// This class is used to parse the ModuleTemplate Config file and check if a module matches one of the configured module templates
+    /// </summary>
+    public class ModuleTemplateParser
+    {
+
+    }
+}
---
+++ ScrapYard/ModuleTemplates/ModuleTemplate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ScrapYard
+{
+    public class ModuleTemplate
+    {
+        public string NameRegex { get; set; }
+        public string[] Requirements { get; set; }
+
+
+        public ModuleTemplate(ConfigNode source)
+        {
+            NameRegex = source.GetValue("name");
+            Requirements = source.GetValues("requirement");
+        }
+
+
+        /// <summary>
+        /// Checks if the provided name matches the template
+        /// </summary>
+        /// <param name="name">Name to check</param>
+        /// <returns>True if the name matches the template</returns>
+        public bool NameMatches(string name)
+        {
+            return Regex.IsMatch(name, NameRegex);
+        }
+
+        public bool Matches(ConfigNode module)
+        {
+            if (!NameMatches(module.GetValue("name")))
+            {
+                return false;
+            }
+
+            //check requirements
+            foreach (string requirement in Requirements)
+            {
+                if (!processRequirement(module, requirement))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        private string getFieldValue(ConfigNode sourceNode, string fieldName)
+        {
+            string[] fieldSplit = fieldName.Split('.');
+
+            ConfigNode currentNode = sourceNode;
+            int i = 0;
+            for (; i<fieldSplit.Length-1; i++)
+            {
+                currentNode = currentNode.GetNode(fieldSplit[i]);
+            }
+
+            string final = fieldSplit[i];
+            
+            if (string.Equals(final, "COUNT", StringComparison.Ordinal))
+            {
+                return (currentNode.CountNodes + currentNode.CountValues).ToString();
+            }
+            else if (string.Equals(final, "NODECOUNT", StringComparison.Ordinal))
+            {
+                return currentNode.CountNodes.ToString();
+            }
+            else if (string.Equals(final, "VALUECOUNT", StringComparison.Ordinal))
+            {
+                return currentNode.CountValues.ToString();
+            }
+
+            return currentNode.GetValue(final);
+        }
+
+        private bool processRequirement(ConfigNode sourceNode, string requirement)
+        {
+            List<string> usedVariables = variablesUsed(requirement);
+            Dictionary<string, string> variables = new Dictionary<string, string>();
+            foreach (string variable in usedVariables)
+            {
+                variables.Add(variable, getFieldValue(sourceNode, variable));
+            }
+
+            //call upon the powers of MagiCore to do some parsing
+            //right now this just uses the Math Parser, but I'd like to have string comparisons as well
+
+            double result = MagiCore.MathParsing.ParseMath("SY_REQUIREMENT_PROCESSING", requirement, variables);
+
+            return (result != 0);
+        }
+
+        private List<string> variablesUsed(string requirementString)
+        {
+            List<string> used = new List<string>();
+            //find everything within square brackets
+            MatchCollection matches = Regex.Matches(requirementString, "\\[.+?\\]");
+            foreach (Match match in matches)
+            {
+                used.Add(match.Value.Trim('[', ']'));
+            }
+
+            return used;
+        }
+    }
+}
