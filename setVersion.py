51042243e895c6a6c0a61b0c260d658b69c4f9f9
magico13 <magico1313@gmail.com>
2018-03-18 21:29:14 -0400

    Change versioning so it is defined by the .version file and not part of the build. Version 1.1 since we added new functionality.

--- setVersion.py
+++ setVersion.py
@@ -8,37 +8,48 @@ VersionInfo = "./ScrapYard/Properties/VersionInfo.cs"
 VersionFile = "./GameData/ScrapYard/ScrapYard.version"
 
 
+with open(VersionFile, 'r') as v:
+    fileJSON = json.load(v)
+
 #get the various parts of the version from the passed in version
-version = sys.argv[1] #"1.2.3.4"  or "major.minor.patch.build"
-versionSplit = version.split('.')
+#version = sys.argv[1] #"1.2.3.4"  or "major.minor.patch.build"
+#versionSplit = version.split('.')
 
-major = int(versionSplit[0])
-minor = int(versionSplit[1])
-patch = int(versionSplit[2])
-build = int(versionSplit[3])
+#major = int(versionSplit[0])
+#minor = int(versionSplit[1])
+#patch = int(versionSplit[2])
+#build = int(versionSplit[3])
 
+build = sys.argv[1]
+
+major = fileJSON["VERSION"]["MAJOR"]
+minor = fileJSON["VERSION"]["MINOR"]
+patch = fileJSON["VERSION"]["PATCH"]
+
+version = '{0}.{1}.{2}.{3}'.format(major, minor, patch, build)
+
+print('Version is '+version+'\n')
 
 #update the VersionInfo.cs file
 print("Setting .dll version")
 with open(VersionInfo, 'w') as v:
-  #v.write('[assembly: System.Reflection.AssemblyVersion("' + version + '")] //Added by build\n')
-  v.write('[assembly: System.Reflection.AssemblyFileVersion("' + version + '")] //Added by build\n')
-  v.write('[assembly: System.Reflection.AssemblyInformationalVersion("' + version + '")] //Added by build\n')
+    #v.write('[assembly: System.Reflection.AssemblyVersion("' + version + '")] //Added by build\n')
+    v.write('[assembly: System.Reflection.AssemblyFileVersion("' + version + '")] //Added by build\n')
+    v.write('[assembly: System.Reflection.AssemblyInformationalVersion("' + version + '")] //Added by build\n')
   
 #Update the .version file
 #don't erase everything, just the version stuff
 #it's json, so lets do it that way
 print("Editing .version file")
 
-with open(VersionFile, 'r') as v:
-  fileJSON = json.load(v)
-
-fileJSON["VERSION"]["MAJOR"] = major
-fileJSON["VERSION"]["MINOR"] = minor
-fileJSON["VERSION"]["PATCH"] = patch
 fileJSON["VERSION"]["BUILD"] = build
 
 with open(VersionFile, 'w') as v:
-  json.dump(fileJSON, v, sort_keys=True, indent=4)
+    json.dump(fileJSON, v, sort_keys=True, indent=4)
   
+
+print("Writing version.txt")
+with open('version.txt', 'w') as f:
+    f.write(version+'\n')
+
 print("Done!")
